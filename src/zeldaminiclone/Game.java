a42c0d434c3f53f2527ee567a5088ca787f39d23
Fabio Silva Ferreira <fabiosvf@gmail.com>
2026-01-20 23:23:09 -0300

    feat: 8. Sistema de tiro

--- src/zeldaminiclone/Game.java
+++ src/zeldaminiclone/Game.java
@@ -98,6 +98,10 @@ public class Game extends Canvas implements Runnable, KeyListener {
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			player.down = true;
 		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_Z) {
+			player.shoot = true;
+		}
 	}
 
 	@Override
--- src/zeldaminiclone/Player.java
+++ src/zeldaminiclone/Player.java
@@ -2,6 +2,8 @@ package zeldaminiclone;
 
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Rectangle {
 
@@ -11,6 +13,10 @@ public class Player extends Rectangle {
 	public int curAnimation = 0;
 	public int curFrames = 0, targetFrames = 15;
 	
+	public static List<Bullet> bullets = new ArrayList<Bullet>();
+	public boolean shoot = false;
+	public int dir = 1;
+	
 	public Player(int x, int y) {
 		super(x,y,32,32);
 	}
@@ -19,9 +25,11 @@ public class Player extends Rectangle {
 		if (right && World.isFree(x + spd, y)) {
 			x+=spd;
 			moved = true;
+			dir = 1;
 		} else if (left && World.isFree(x - spd, y)) {
 			x-=spd;
 			moved = true;
+			dir = -1;
 		} 
 		
 		if (up && World.isFree(x, y - spd)) {
@@ -42,9 +50,22 @@ public class Player extends Rectangle {
 				}
 			}
 		}
+		
+		if (shoot) {
+			shoot = false;
+			bullets.add(new Bullet(x, y, dir));
+		}
+		
+		for (int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).tick();
+		}
 	}
 	
 	public void render(Graphics g) {
 		g.drawImage(Spritesheet.playersFront[curAnimation], x, y, 32, 32, null);
+		
+		for (int i = 0; i < bullets.size(); i++) {
+			bullets.get(i).render(g);
+		}
 	}
 }
